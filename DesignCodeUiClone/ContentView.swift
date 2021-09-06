80e50be49b8d663c8c0f84ab38f4675328c48c33
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2021-09-06 20:00:45 +0530

    Initial Commit

---
+++ DesignCodeUiClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeUiClone
+//
+//  Created by KANISHK VIJAYWARGIYA on 06/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCodeUiClone/DesignCodeUiCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeUiCloneApp.swift
+//  DesignCodeUiClone
+//
+//  Created by KANISHK VIJAYWARGIYA on 06/09/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeUiCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
